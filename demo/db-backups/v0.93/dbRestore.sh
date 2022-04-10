e6a000a4084850e3bbc6486f918cefd8db545a36
rohit-yawalkar <rohit.yawalkar@thoughtworks.com>
2022-04-10 14:25:15 +0530

    BAH-1315 - | Rohit | - Refactors db reset scripts for demo-v0.93

--- demo/db-backups/v0.93/dbRestore.sh
+++ demo/db-backups/v0.93/dbRestore.sh
@@ -31,6 +31,8 @@ download_and_unzip(){
   wget -O $DEST_LOCATION/$OPENMRS_SQL_FILE $GITHUB_BASE_URL/$OPENMRS_SQL_FILE
   wget -O $DEST_LOCATION/$BAHMNIPACS_SQL_FILE $GITHUB_BASE_URL/$BAHMNIPACS_SQL_FILE
   wget -O $DEST_LOCATION/$PACSDB_SQL_FILE $GITHUB_BASE_URL/$PACSDB_SQL_FILE
+
+  gzip -d $DEST_LOCATION/$OPENELIS_SQL_FILE.gz $DEST_LOCATION/$ODOO_SQL_FILE.gz $DEST_LOCATION/$OPENMRS_SQL_FILE.gz $DEST_LOCATION/$BAHMNIPACS_SQL_FILE.gz $DEST_LOCATION/$PACSDB_SQL_FILE.gz
 }
 
 restore(){
@@ -39,30 +41,30 @@ restore(){
 
   mysql -u$SQLUSER -p$PASSWORD -e "drop database $OPENMRS_DB_NAME"
   mysql -u$SQLUSER -p$PASSWORD -e "create database $OPENMRS_DB_NAME"
-  mysql -u$SQLUSER -p$PASSWORD $OPENMRS_DB_NAME < $OPENMRS_SQL_FILE
+  mysql -u$SQLUSER -p$PASSWORD $OPENMRS_DB_NAME < $DEST_LOCATION/$OPENMRS_SQL_FILE
   mysql -u$SQLUSER -p$PASSWORD -e "FLUSH PRIVILEGES"
 
   ps aux | grep -ie $OPENELIS_DB_NAME | awk '{print $2}' | xargs kill -9
   psql -U$PSQLUSER -c "drop database if exists $OPENELIS_DB_NAME;"
   psql -U$PSQLUSER -c "create database $OPENELIS_DB_NAME;"
-  psql -U$PSQLUSER $OPENELIS_DB_NAME < $OPENELIS_SQL_FILE
+  psql -U$PSQLUSER $OPENELIS_DB_NAME < $DEST_LOCATION/$OPENELIS_SQL_FILE
   psql -U$PSQLUSER -c "ALTER DATABASE $OPENELIS_DB_NAME OWNER TO $CLINLIMSUSER;"
 
   ps aux | grep -ie $ODOO_DB_NAME | awk '{print $2}' | xargs kill -9
   psql -U$PSQLUSER -c  "drop database if exists $ODOO_DB_NAME;"
   psql -U$PSQLUSER -c  "create database $ODOO_DB_NAME;"
-  psql -U$PSQLUSER $ODOO_DB_NAME < $ODOO_SQL_FILE
+  psql -U$PSQLUSER $ODOO_DB_NAME < $DEST_LOCATION/$ODOO_SQL_FILE
   psql -U$PSQLUSER -c "ALTER DATABASE $ODOO_DB_NAME OWNER TO $ODOOUSER;"
 
   ps aux | grep -ie $BAHMNIPACS_DB_NAME | awk '{print $2}' | xargs kill -9
   psql -U$PSQLUSER -c  "drop database if exists $BAHMNIPACS_DB_NAME;"
-  psql -U$PSQLUSER -c  "create database $BAHMNIPACS_DB_NAME;"
+  psql -U$PSQLUSER -c  "create database $DEST_LOCATION/$BAHMNIPACS_DB_NAME;"
   psql -U$PSQLUSER $BAHMNIPACS_DB_NAME < $BAHMNIPACS_SQL_FILE
 
   ps aux | grep -ie $PACS_DB_NAME | awk '{print $2}' | xargs kill -9
   psql -U$PSQLUSER -c  "drop database if exists $PACS_DB_NAME;"
   psql -U$PSQLUSER -c  "create database $PACS_DB_NAME;"
-  psql -U$PSQLUSER $PACS_DB_NAME < $PACSDB_SQL_FILE
+  psql -U$PSQLUSER $PACS_DB_NAME < $DEST_LOCATION/$PACSDB_SQL_FILE
 
   bahmni -i local start
 }
